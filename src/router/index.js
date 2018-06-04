2a24eeaab1009d278541e3a632ff42710ee54897
ruoyu <hunger@jirengu.com>
2018-06-04 15:20:19 +0800

    路由 权限 异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+/*
 import Index from '@/pages/Index/template.vue'
 import Login from '@/pages/Login/template.vue'
 import Detail from '@/pages/Detail/template.vue'
@@ -8,10 +9,15 @@ import Create from '@/pages/Create/template.vue'
 import Register from '@/pages/Register/template.vue'
 import User from '@/pages/User/template.vue'
 import My from '@/pages/My/template.vue'
+*/
+
+import store from '../store'
+window.store = store
 
 Vue.use(Router)
 
-export default new Router({
+/*
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -22,28 +28,92 @@ export default new Router({
       component: Login
     },
     {
-      path: '/detail',
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '/detail/:blogId',
       component: Detail
     },
     {
-      path: '/edit',
-      component: Edit
+      path: '/edit/:blogId',
+      component: Edit,
+      meta: { requiresAuth: true }
     },
     {
       path: '/create',
-      component: Create
+      component: Create,
+      meta: { requiresAuth: true }
     },
     {
-      path: '/user',
+      path: '/user/:userId',
       component: User
     },
     {
       path: '/my',
-      component: My
-    },
-    {
-      path: '/register',
-      component: Register
+      component: My,
+      meta: { requiresAuth: true }
     }
   ]
 })
+*/
+
+const router =  new Router({
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/pages/Index/template.vue')
+    },
+    {
+      path: '/login',
+      component: () => import('@/pages/Login/template.vue')
+    },
+    {
+      path: '/detail/:blogId',
+      component: () => import('@/pages/Detail/template.vue')
+    },
+    {
+      path: '/edit/:blogId',
+      component: () => import('@/pages/Edit/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/create',
+      component: () => import('@/pages/Create/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/user/:userId',
+      component: () => import('@/pages/User/template.vue')
+    },
+    {
+      path: '/my',
+      component: () => import('@/pages/My/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/register',
+      component: () => import('@/pages/Register/template.vue')
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    store.dispatch('checkLogin').then(isLogin=>{
+      if (!isLogin) {
+        next({
+          path: '/login',
+          query: { redirect: to.fullPath }
+        })
+      } else {
+        next()
+      }    
+    })
+  } else {
+    next() // 确保一定要调用 next()
+  }
+})
+
+export default router
+
